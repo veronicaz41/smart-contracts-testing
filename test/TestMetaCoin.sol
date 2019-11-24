99fd1527f6534224f2232d3a0137c1d75f36149d
Veronica Zheng <veronica.zheng@coinbase.com>
2019-11-24 15:38:09 -0800

    add truffle tests

---
+++ test/TestMetaCoin.sol
@@ -0,0 +1,35 @@
+// From https://github.com/truffle-box/metacoin-box
+
+pragma solidity ^0.5.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/truffle-examples/MetaCoin.sol";
+
+contract TestMetaCoin {
+    function testInitialBalanceUsingDeployedContract() public {
+        MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
+
+        uint256 expected = 10000;
+
+        Assert.equal(
+            meta.getBalance(msg.sender),
+            expected,
+            "Owner should have 10000 MetaCoin initially"
+        );
+    }
+
+    function testInitialBalanceWithNewMetaCoin() public {
+        address owner = msg.sender;
+        MetaCoin meta = new MetaCoin(owner);
+
+        uint256 expected = 10000;
+
+        Assert.equal(
+            meta.getBalance(owner),
+            expected,
+            "Owner should have 10000 MetaCoin initially"
+        );
+    }
+
+}
