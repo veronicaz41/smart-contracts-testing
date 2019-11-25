e1adb0b2de15efc23c8668b61956bf68d7218078
Veronica Zheng <veronica.zheng@coinbase.com>
2019-11-25 09:22:57 -0800

    Fix suicidal

--- contracts/slither-examples/Suicidal.sol
+++ contracts/slither-examples/Suicidal.sol
@@ -4,7 +4,9 @@
 pragma solidity ^0.5.0;
 
 contract Suicidal {
-    function kill() external {
+    constructor() public payable {}
+
+    function kill() public {
         selfdestruct(msg.sender);
     }
 }
